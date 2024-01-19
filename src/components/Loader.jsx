ecf2c18fbf676fa339f7a2260253081848eba6c0
Mariana Téllez <84681760+MarianaTellezR@users.noreply.github.com>
2024-01-19 12:29:30 -0600

    Updated Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import AnimatedLogo from "../assets/images/logo-animated.gif";
+// import AnimatedLogo from "../assets/images/logo-animated.gif";
+import PepsiLoader from "../assets/images/pepsi.gif";
 
 function Loader() {
     return ( 
         <div className='loader'>
-            <img className='logo' src={AnimatedLogo} alt='apple loader' />
+            <img className='logo' src={PepsiLoader} alt='apple loader' />
         </div>
      );
 }

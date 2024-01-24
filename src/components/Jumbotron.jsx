0097278ced3c5564e06beb8a99fcf353954d7431
Mariana Téllez <84681760+MarianaTellezR@users.noreply.github.com>
2024-01-24 13:59:28 -0600

    Removed image

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -14,7 +14,7 @@ function Jumbotron() {
     return ( 
         <div className='jumbotron-section wrapper'>
             <h2 className='title'>New</h2>
-            <img className='logo' src={Iphone} alt="IPhone 14 Pro" />
+            
             <p className='text'>Big and bigger.</p>
             <span className='description'>
                 From $41.62/mo. for 24 mo. or $999 before trade-in
